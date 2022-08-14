0fae4757f9070735773bff57d7e40516a84f2d3c
AegisVP <vlad@pysarenko.com>
2022-08-14 23:29:27 +0200

    refactor ContactForm to hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import { SubmitButton, ResetButton, Label, InputField } from './ContactForm.styled';
 import { Box } from 'components/Common/Box.styled';
 
@@ -8,82 +7,162 @@ const initialValues = { id: '', name: '', number: '' };
 const addButtonText = 'Add user';
 const editButtonText = 'Update user';
 
-export class ContactForm extends Component {
-  state = initialValues;
+export const ContactForm = ({ editId, editName, editNumber, onSubmit, onResetForm }) => {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.editId !== this.props.editId && prevState.id === this.state.id) {
-      const { editId, editName, editNumber } = this.props;
-      this.setState({ id: editId, name: editName, number: editNumber });
-    }
-  }
+  useEffect(() => {
+    setId(editId);
+    setName(editName);
+    setNumber(editNumber);
+  }, [editId, editName, editNumber]);
 
-  contactSubmitHandler = e => {
+  const setInitialValues = () => {
+    setId(initialValues.id);
+    setName(initialValues.name);
+    setNumber(initialValues.number);
+  };
+
+  const contactSubmitHandler = e => {
     e.preventDefault();
     const { id, name, number } = e.target.elements;
-    if (this.props.onSubmit({ id: id.value, name: name.value, number: number.value })) this.setState(initialValues);
+    if (onSubmit({ id: id.value, name: name.value, number: number.value })) setInitialValues();
   };
 
-  onResetForm = () => {
-    this.setState(initialValues);
-    this.props.onResetForm();
+  const onResetLocalForm = () => {
+    setInitialValues();
+    onResetForm();
   };
 
-  handleChange = e => {
-    this.setState({ [e?.currentTarget?.name]: e?.currentTarget?.value });
-  };
-
-  render() {
-    if (this.props.editId && !this.state.id) {
-      const { editId, editName, editNumber } = this.props;
-      this.setState({ id: editId, name: editName, number: editNumber });
+  const handleChange = e => {
+    const value = e?.currentTarget?.value;
+    switch (e?.currentTarget?.name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
     }
+  };
 
-    return (
-      <form action="#" onSubmit={this.contactSubmitHandler}>
-        <input name="id" defaultValue={this.state.id} hidden />
-        <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
-          <Label htmlFor="contactName">Name</Label>
-          <InputField
-            id="contactName"
-            type="text"
-            name="name"
-            value={this.state.name}
-            title="Enter your name"
-            required
-            onChange={this.handleChange}
-          />
-        </Box>
-        <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
-          <Label htmlFor="contactNumber">Phone number</Label>
-          <InputField
-            type="tel"
-            name="number"
-            value={this.state.number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,15}"
-            title="Phone number must be up to 15 digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-          />
-        </Box>
-        {this.props.editId ? (
-          <Box display="flex">
-            <SubmitButton type="submit">{editButtonText}</SubmitButton>
-            <ResetButton type="reset" onClick={this.onResetForm}>
-              🔙
-            </ResetButton>
-          </Box>
-        ) : (
-          <SubmitButton type="submit">{addButtonText}</SubmitButton>
-        )}
-        Classic form
-      </form>
-    );
+  if (editId && !id) {
+    setId(editId);
+    setName(editName);
+    setNumber(editNumber);
   }
-}
+
+  return (
+    <form action="#" onSubmit={contactSubmitHandler}>
+      <input name="id" defaultValue={id} hidden />
+      <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
+        <Label htmlFor="contactName">Name</Label>
+        <InputField
+          id="contactName"
+          type="text"
+          name="name"
+          value={name}
+          title="Enter your name"
+          required
+          onChange={handleChange}
+        />
+      </Box>
+      <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
+        <Label htmlFor="contactNumber">Phone number</Label>
+        <InputField
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,15}"
+          title="Phone number must be up to 15 digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+        />
+      </Box>
+      {editId ? (
+        <Box display="flex">
+          <SubmitButton type="submit">{editButtonText}</SubmitButton>
+          <ResetButton type="reset" onClick={onResetLocalForm}>
+            🔙
+          </ResetButton>
+        </Box>
+      ) : (
+        <SubmitButton type="submit">{addButtonText}</SubmitButton>
+      )}
+      Classic form
+    </form>
+  );
+};
+
+// export class OldContactForm extends Component {
+//   state = initialValues;
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevProps.editId !== this.props.editId && prevState.id === this.state.id) {
+//       const { editId, editName, editNumber } = this.props;
+//       this.setState({ id: editId, name: editName, number: editNumber });
+//     }
+//   }
+//   contactSubmitHandler = e => {
+//     e.preventDefault();
+//     const { id, name, number } = e.target.elements;
+//     if (this.props.onSubmit({ id: id.value, name: name.value, number: number.value })) this.setState(initialValues);
+//   };
+//   onResetForm = () => {
+//     this.setState(initialValues);
+//     this.props.onResetForm();
+//   };
+//   handleChange = e => {
+//     this.setState({ [e?.currentTarget?.name]: e?.currentTarget?.value });
+//   };
+//   render() {
+//     if (this.props.editId && !this.state.id) {
+//       const { editId, editName, editNumber } = this.props;
+//       this.setState({ id: editId, name: editName, number: editNumber });
+//     }
+//     return (
+//       <form action="#" onSubmit={this.contactSubmitHandler}>
+//         <input name="id" defaultValue={this.state.id} hidden />
+//         <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
+//           <Label htmlFor="contactName">Name</Label>
+//           <InputField
+//             id="contactName"
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             title="Enter your name"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </Box>
+//         <Box display="flex" flexDirection="column" mt="10px" p="0" border="1px solid #888888" borderRadius="2px">
+//           <Label htmlFor="contactNumber">Phone number</Label>
+//           <InputField
+//             type="tel"
+//             name="number"
+//             value={this.state.number}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,15}"
+//             title="Phone number must be up to 15 digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </Box>
+//         {this.props.editId ? (
+//           <Box display="flex">
+//             <SubmitButton type="submit">{editButtonText}</SubmitButton>
+//             <ResetButton type="reset" onClick={this.onResetForm}>
+//               🔙
+//             </ResetButton>
+//           </Box>
+//         ) : (
+//           <SubmitButton type="submit">{addButtonText}</SubmitButton>
+//         )}
+//         Classic form
+//       </form>
+//     );
+//   }
+// }
 
 // ❌✍️👎👍🛑⛔🔙
-
-ContactForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
